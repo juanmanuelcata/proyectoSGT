1a1cc0313c8e9acf6b8b93d1f78391ea8b3d1225
PatrickGardner <patricio-r-g@hotmail.com>
2013-07-19 16:19:48 -0300

    Configuracion de pato

--- abm/medico/_ver_detalle.php
+++ abm/medico/_ver_detalle.php
@@ -10,7 +10,7 @@ $campos = $result->fetch(PDO::FETCH_ASSOC);
 ?>
 
 
-<label style="default"><strong>Nombre: </strong><?php echo ($campos['nombre']) ?></label>
+    <label><strong>Nombre: </strong><?php echo ($campos['nombre']) ?></label>
     <br>
     <label><strong>Apellido: </strong><?php echo ($campos['apellido']) ?></label>
     <br>
