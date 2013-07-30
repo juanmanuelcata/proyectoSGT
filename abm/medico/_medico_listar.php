f6b9026e44c33c256132f534981c91c1054e0884
patricio-r-g@hotmail.com <patricio-r-g@hotmail.com>
2013-07-30 01:16:12 -0300

    ayudas en el reporte de medico

--- abm/medico/_medico_listar.php
+++ abm/medico/_medico_listar.php
@@ -61,7 +61,7 @@ $result = $db->query($consulta);
         <i class="icon-question-sign"></i>
     </button>
     <button type="submit" class="btn btn-success offset1">Imprimir</button>
-    <button class="btn btn-mini" id='ayuda' onclick="return false;" data-original-title="Imprimir" data-content="Seleccione los Médicos que desee imprimir haciendo click sobre el campo 'Elegir' de cada uno o utilizando el botón de 'Marcar todos'. Una vez hecho esto oprima el botón 'Imprimir' y se abrirá una nueva ventana con un documento pdf el cual podrá imprimir utilizando las opciones del navegador que esté usando.">
+    <button class="btn btn-mini" id='ayuda' onclick="return false;" data-original-title="Imprimir" data-content="Seleccione los Médicos que desee imprimir haciendo click sobre el campo 'Elegir' de cada uno o utilizando el botón de 'Marcar todos'. Una vez hecho esto oprima el botón 'Imprimir' y se abrirá una nueva ventana con un documento pdf, el cual podrá imprimir utilizando las opciones del navegador que esté usando.">
         <i class="icon-question-sign"></i>
     </button>
 </form>
--- abm/medico/_medico_reportes.php
+++ abm/medico/_medico_reportes.php
@@ -3,12 +3,18 @@ $db = conectaDb();
 $consulta = "select * from medico";
 $result = $db->query($consulta);
 ?>
-<legend>Turnos por médico</legend>
+<legend>Turnos por Médico</legend>
 <label>Fecha inicio</label>
-<input class="fecha" type="date" tabindex="1" class="input-large" id="fechaInic" name="fechaInic">           
+<input class="fecha" type="date" tabindex="1" class="input-large" id="fechaInic" name="fechaInic">
+<button class="btn btn-mini" onclick="return false;" data-original-title="Fecha de inicio del intervalo" data-content="Increse la fecha de inicio del intervalo en el que desee ver los reportes, utilizando solo caracteres numéricos. Puede también utilizar la flecha hacia abajo para desplegar el calendario y oprimiendo sobre el día correspondiente al mes y año que esten visualizados. Puede optar por seleccionar un campo de la fecha (dia, mes o año) haciendo click sobre el mismo y con las flechas hacia arriba y abajo escribir la fecha deseada. Puede borrar una fecha con 'x'.">
+    <i class="icon-question-sign"></i>
+</button>
 <br><br>
 <label>Fecha fin</label>
 <input class="fecha" type="date" tabindex="2" class="input-large" id="fechaFin" name="fechaFin" onkeypress="return justFecha(event);">
+<button class="btn btn-mini" onclick="return false;" data-original-title="Fecha de fin del intervalo" data-content="Increse la fecha de fin del intervalo en el que desee ver los reportes, utilizando solo caracteres numéricos. Puede también utilizar la flecha hacia abajo para desplegar el calendario y oprimiendo sobre el día correspondiente al mes y año que esten visualizados. Puede optar por seleccionar un campo de la fecha (dia, mes o año) haciendo click sobre el mismo y con las flechas hacia arriba y abajo escribir la fecha deseada. Puede borrar una fecha con 'x'.">
+    <i class="icon-question-sign"></i>
+</button>
 <br><br>
 <form class="form-horizontal" name="form1" action="./medico/_medico_imprimir_reportes.php" method="GET" target="_blank">
     <div class="control-group">
@@ -17,10 +23,18 @@ $result = $db->query($consulta);
                 <table id="tabla1" class="table table-striped">
                     <thead>
                         <tr>
-                            <th>Elegir</th>
-                            <th>Médico</th>
-                            <th>Cantidad de turnos</th>
-                            <th>% de turnos</th>
+                            <th>Elegir <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Elegir para impresión" data-content="Oprima sobre cada Médico para seleccionarlo, o utilice los botones de Marcar o Desmarcar todos.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
+                            <th>Médico <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Médico" data-content="Oprima sobre este campo para ordenar la lista por el Nombre y Apellido del Médico. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
+                            <th>Cantidad de turnos <button class="btn btn-mini" onclick="return false;" data-original-title="Campo cantidad de turnos" data-content="Oprima sobre este campo para ordenar la lista por la cantidad de turnos asignados a cada Médico. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
+                            <th>% de turnos <button class="btn btn-mini" onclick="return false;" data-original-title="Campo % de turnos" data-content="Oprima sobre este campo para ordenar la lista por el porcentaje de turnos asignados a cada Médico. El porcentaje de turnos es sobre el total global de turnos asignados. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -57,9 +71,15 @@ $result = $db->query($consulta);
     </div>
     <br>
     <a href="javascript:seleccionar_todo1()">Marcar todos</a> |
-    <a href="javascript:deseleccionar_todo1()">Desmarcar todos</a> 
+    <a href="javascript:deseleccionar_todo1()">Desmarcar todos</a>
+    <button class="btn btn-mini" onclick="return false;" data-original-title="Opciones de marcado" data-content="Oprima 'Marcar todos' para seleccionar todos los Reportes visualizados u oprima 'Desmarcar todos' para desmarcar todos los Médicos que estén visualizados.">
+        <i class="icon-question-sign"></i>
+    </button>
     <input type="hidden" name="code" value="1"/>
     <button type="submit" class="btn btn-success offset1">Imprimir</button>
+    <button class="btn btn-mini" id='ayuda' onclick="return false;" data-original-title="Imprimir" data-content="Seleccione los Reportes que desee imprimir haciendo click sobre el campo 'Elegir' de cada uno o utilizando el botón de 'Marcar todos'. Una vez hecho esto oprima el botón 'Imprimir' y se abrirá una nueva ventana con un documento pdf, el cual podrá imprimir utilizando las opciones del navegador que esté usando.">
+        <i class="icon-question-sign"></i>
+    </button>
 </form>
 
 <script>
@@ -102,10 +122,16 @@ $result = $db->query($consulta);
 
 <legend>Turnos cancelados</legend>
 <label>Fecha inicio</label>
-<input class="fecha1" type="date" tabindex="1" class="input-large" id="fechaInic1" name="fechaInic1">           
+<input class="fecha1" type="date" tabindex="1" class="input-large" id="fechaInic1" name="fechaInic1">
+<button class="btn btn-mini" onclick="return false;" data-original-title="Fecha de inicio del intervalo" data-content="Increse la fecha de inicio del intervalo en el que desee ver los reportes, utilizando solo caracteres numéricos. Puede también utilizar la flecha hacia abajo para desplegar el calendario y oprimiendo sobre el día correspondiente al mes y año que esten visualizados. Puede optar por seleccionar un campo de la fecha (dia, mes o año) haciendo click sobre el mismo y con las flechas hacia arriba y abajo escribir la fecha deseada. Puede borrar una fecha con 'x'.">
+    <i class="icon-question-sign"></i>
+</button>
 <br><br>
 <label>Fecha fin</label>
 <input class="fecha1" type="date" tabindex="2" class="input-large" id="fechaFin1" name="fechaFin1">
+<button class="btn btn-mini" onclick="return false;" data-original-title="Fecha de fin del intervalo" data-content="Increse la fecha de fin del intervalo en el que desee ver los reportes, utilizando solo caracteres numéricos. Puede también utilizar la flecha hacia abajo para desplegar el calendario y oprimiendo sobre el día correspondiente al mes y año que esten visualizados. Puede optar por seleccionar un campo de la fecha (dia, mes o año) haciendo click sobre el mismo y con las flechas hacia arriba y abajo escribir la fecha deseada. Puede borrar una fecha con 'x'.">
+    <i class="icon-question-sign"></i>
+</button>
 <br><br>
 <form class="form-horizontal" name="form2" action="./medico/_medico_imprimir_reportes.php" method="GET" target="_blank">
     <div class="control-group">
@@ -114,10 +140,18 @@ $result = $db->query($consulta);
                 <table id="tabla2" class="table table-striped">
                     <thead>
                         <tr>
-                            <th>Elegir</th>
-                            <th>Médico</th>
-                            <th>Turnos cancelados</th>
-                            <th>% de turnos</th>
+                            <th>Elegir <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Elegir para impresión" data-content="Oprima sobre cada Médico para seleccionarlo, o utilice los botones de Marcar o Desmarcar todos.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
+                            <th>Médico <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Médico" data-content="Oprima sobre este campo para ordenar la lista por el Nombre y Apellido del Médico. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
+                            <th>Turnos cancelados <button class="btn btn-mini" onclick="return false;" data-original-title="Campo cantidad de turnos cancelados" data-content="Oprima sobre este campo para ordenar la lista por la cantidad de turnos cancelados de cada Médico. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
+                            <th>% de turnos <button class="btn btn-mini" onclick="return false;" data-original-title="Campo % de turnos" data-content="Oprima sobre este campo para ordenar la lista por el porcentaje de turnos asignados a cada Médico. El porcentaje de turnos es sobre el total de turnos asignados a cada Médico. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -155,8 +189,14 @@ $result = $db->query($consulta);
     <br>
     <a href="javascript:seleccionar_todo2()">Marcar todos</a> | 
     <a href="javascript:deseleccionar_todo2()">Desmarcar todos</a>
+    <button class="btn btn-mini" onclick="return false;" data-original-title="Opciones de marcado" data-content="Oprima 'Marcar todos' para seleccionar todos los Reportes visualizados u oprima 'Desmarcar todos' para desmarcar todos los Médicos que estén visualizados.">
+        <i class="icon-question-sign"></i>
+    </button>
     <input type="hidden" name="code" value="2"/>
     <button type="submit" class="btn btn-success offset1">Imprimir</button>
+    <button class="btn btn-mini" id='ayuda' onclick="return false;" data-original-title="Imprimir" data-content="Seleccione los Reportes que desee imprimir haciendo click sobre el campo 'Elegir' de cada uno o utilizando el botón de 'Marcar todos'. Una vez hecho esto oprima el botón 'Imprimir' y se abrirá una nueva ventana con un documento pdf, el cual podrá imprimir utilizando las opciones del navegador que esté usando.">
+        <i class="icon-question-sign"></i>
+    </button>
 </form>
 
 <div><strong>Nota: </strong>el porcentaje de turnos cancelados es sobre el total del turnos del médico</div>
@@ -197,9 +237,9 @@ $result = $db->query($consulta);
 
 
 <?php
-    $conhorarios = "select id, nombre, apellido, dia, min(desde) desde, max(hasta) hasta, id_med from horario inner join medico on (horario.id_med = medico.idmedico) group by id_med, dia";
-    $result = $db->query($conhorarios);
-    ?>
+$conhorarios = "select id, nombre, apellido, dia, min(desde) desde, max(hasta) hasta, id_med from horario inner join medico on (horario.id_med = medico.idmedico) group by id_med, dia";
+$result = $db->query($conhorarios);
+?>
 
 <legend>Medicos por banda horaria</legend>
 <label>Horario inicio</label>
@@ -229,11 +269,21 @@ $result = $db->query($consulta);
                 <table id="tabla3" class="table table-striped">
                     <thead>
                         <tr>
-                            <th>Elegir</th>
-                            <th>Médico</th>
-                            <th>día</th>
-                            <th>Desde</th>
-                            <th>Hasta</th>
+                            <th>Elegir <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Elegir para impresión" data-content="Oprima sobre cada Médico para seleccionarlo, o utilice los botones de Marcar o Desmarcar todos.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
+                            <th>Médico <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Médico" data-content="Oprima sobre este campo para ordenar la lista por el Nombre y Apellido del Médico. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                                    <i class="icon-question-sign"></i>
+                                </button></th>
+                            <th>Día <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Día" data-content="Oprima sobre este campo para ordenar la lista por el atributo Día de la consulta. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                            <i class="icon-question-sign"></i>
+                        </button></th>
+                            <th>Desde <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Desde" data-content="Oprima sobre este campo para ordenar la lista por la hora de inicio de la consulta. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                            <i class="icon-question-sign"></i>
+                        </button></th>
+                            <th>Hasta <button class="btn btn-mini" onclick="return false;" data-original-title="Campo Hasta" data-content="Oprima sobre este campo para ordenar la lista por la hora de finalización de la consulta. Cada vez que se oprime cambiará entre forma ascendente y descendente.">
+                            <i class="icon-question-sign"></i>
+                        </button></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -254,9 +304,18 @@ $result = $db->query($consulta);
     </div
     <br>
     <a href="javascript:seleccionar_todo3()">Marcar todos</a> | 
-    <a href="javascript:deseleccionar_todo3()">Desmarcar todos</a> 
+    <a href="javascript:deseleccionar_todo3()">Desmarcar todos</a>
+    <button class="btn btn-mini" onclick="return false;" data-original-title="Opciones de marcado" data-content="Oprima 'Marcar todos' para seleccionar todos los Reportes visualizados u oprima 'Desmarcar todos' para desmarcar todos los Médicos que estén visualizados.">
+        <i class="icon-question-sign"></i>
+    </button>
     <input type="hidden" name="code" value="3"/>
     <button type="submit" class="btn btn-success offset1">Imprimir</button>
+    <button class="btn btn-mini" id='ayuda' onclick="return false;" data-original-title="Imprimir" data-content="Seleccione los Reportes que desee imprimir haciendo click sobre el campo 'Elegir' de cada uno o utilizando el botón de 'Marcar todos'. Una vez hecho esto oprima el botón 'Imprimir' y se abrirá una nueva ventana con un documento pdf, el cual podrá imprimir utilizando las opciones del navegador que esté usando.">
+        <i class="icon-question-sign"></i>
+    </button>
+    <!--los br's son para que se vea el botón de ayuda-->
+    <br><br><br>
+
 </form>
 
 <script>
